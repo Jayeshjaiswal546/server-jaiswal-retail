6c460cf43921fb1daa17c5e7e6656dd91209d0a0
Jayeshjaiswal546 <139918759+Jayeshjaiswal546@users.noreply.github.com>
2025-04-02 04:29:02 +0530

    Update api.js

--- app/api/api.js
+++ app/api/api.js
@@ -1,1 +1,4 @@
-exports.frontendBaseUrl = 'http://localhost:3000';
+// exports.frontendBaseUrl = 'http://localhost:3000';
+
+exports.frontendBaseUrl = 'https://frontend-jaiswal-retail-rfu1.vercel.app';
+
